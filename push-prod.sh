dbccc62512016cf44ea1c5af1db65124978dd2c4
petercoe <pete@speedpick.com>
2012-09-12 12:08:38 -0700

    Add usage, check arguments and use consistent variable names

--- push-prod.sh
+++ push-prod.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
 
+Usage()
+{
+    echo "Usage: push-prod.sh <Keyname> <file>"
+}
+
 #
 # Push the specified file out to the production hosts for Keyname
 #
 Keyname=$1
 
+if [ $# -ne 2 ]
+then
+    Usage
+    exit 1
+fi
+
 if [ -z "${Keyname}" ]
 then
     echo "You must specify a Keyname"
-    echo "Usage: create-instances.sh <Keyname> [Instances (defaults to 4)]"
+    Usage
     exit 1
 fi
 
@@ -17,10 +28,10 @@ fi
 #
 if [ -f $2 ]
 then
-    for host in `cat ${Keyname}-hosts.txt | sed -e's%.* %%'`
+    for Hostname in `cat ${Keyname}-hosts.txt | sed -e's%.* %%'`
     do
-        echo "\t${host}"
-        scp -p -i ${HOME}/.ssh/${Keyname}.pem $1 ubuntu@${Hostname}:/var/www
+        echo "\t${Hostname}"
+        scp -p -i ${HOME}/.ssh/${Keyname}.pem $2 ubuntu@${Hostname}:/var/www
     done
 else
     echo "$1 does not exist"
