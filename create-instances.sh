7108f16d06c4a317eb6e5568ceb061b233501fcb
petercoe <pete@speedpick.com>
2012-09-12 10:20:23 -0700

    add documentation to the scripts

--- create-instances.sh
+++ create-instances.sh
@@ -1,50 +1,111 @@
 #!/bin/sh
 
+#
+# Script to create a somewhat arbitary number of EC2 instances
+# and place them behind an Elastic Load Balancer
+#
+
 createInstance()
 {
+    #
+    # Create an instance on EC2 
+    #   ami-43ddf806 is a 64 bit Ubuntu 12.04 LTS server preconfigured with
+    #   Apche
+    #
     Instance=`ec2-run-instances ami-43ddf806 --instance-type t1.micro --region us-west-1 -k ${Keyname}.keypair | egrep INSTANCE | awk '{print $2}'`
 
+    # 
+    # Check status of Instance, and wait until the server is up 
+    # and running
+    #
     Seconds=0
-    until [ "`ec2-describe-instance-status ${Instance} | egrep SYSTEMSTATUS | awk '{print $3}'`" == 'passed' ]
+    until [ "`ec2-describe-instance-status ${Instance} | egrep SYSTEMSTATUS | awk '{print $3}'`" = 'passed' ]
     do
         echo "Waiting for startup - ${Seconds} Seconds"
         sleep 5
         Seconds=`expr ${Seconds} + 5`
     done
 
-    ec2addtag --region us-west-1 ${Instance} -t Name=Host-${Host}
+    #
+    # Add a descriptive tag to the server
+    #
+    ec2addtag --region us-west-1 ${Instance} -t Name=${Keyname}-${Host}
     Hostname=`ec2-describe-instances ${Instance} | egrep INSTANCE | awk '{print $4}'`
+    #
+    # Change permissions on the DocumentRoot for easy pushing
+    #
     ssh -i ${HOME}/.ssh/${Keyname}.pem -o "StrictHostKeyChecking no" ubuntu@${Hostname} sudo chown -R ubuntu /var/www
+
+    #
+    # Copy the html up to the server, and tag it with hostname
+    #
     sed "s%HOSTNAME%${Hostname}%g" < home.shtml > home.html
     scp -p -i ${HOME}/.ssh/${Keyname}.pem home.html ubuntu@${Hostname}:/var/www
     echo ${Instance} ${Hostname} >> hosts.txt
-    echo "Host-${Host} - ${Hostname} Complete"
+    echo "${Keyname}-${Host} - ${Hostname} Complete"
 }
 
+#
+# MAIN
+#
 Keyname=$1
 Instances=$2
-loadBalancer=infra-push
+
+if [ -z "${EC2_KEYPAIR_NAME}" ]
+then
+    echo "Error: Set $EC2_KEYPAIR_NAME in environment to your registered keypair from create-keypair.sh"
+fi
 
 if [ -z "${Keyname}" ]
 then
     echo "You must specify a key name"
+    echo "Usage: create-instances.sh <Keyname> [Instances (defaults to 4)]"
     exit 1
 fi
 
 if [ -z "${Instances}" ]
 then
-    Instances=0
+    Instances=4
 fi
 
+loadBalancer=${Keyname}
 Host=1
 cat /dev/null > hosts.txt
-rm home.html
+rm -f home.html
 
+#
+# Create the specified number of instances
+#
 while [ ${Host} -le ${Instances} ]
 do
     createInstance ${Keyname} ${Host} 
     Host=`expr ${Host} + 1`
 done
+
+#
+# Create the Elastic Load Balancer
+#
 instanceIds=`cat hosts.txt | sed -e's% .*%,%'`
-elb-create-lb ${loadBalancer} -region us-west-1 -availability-zones us-west-1a,us-west-1b -listener "protocol=http,lb-port=80,instance-port=80"
-elb-register-instances-with-lb ${loadBalancer} -region us-west-1 -instances ${instanceIds}
+loadBalancerDNSName=`elb-create-lb ${loadBalancer} --region us-west-1 --availability-zones us-west-1a,us-west-1b --listener "protocol=http,lb-port=80,instance-port=80" | sed 's%.* %%'`
+
+#
+# Register Instances with Load Balancer
+#
+echo "Creating Loadbalancer: ${loadBalancer}"
+elb-register-instances-with-lb ${loadBalancer} --region us-west-1 --instances ${instanceIds}
+
+echo "Waiting for Loadbalancer to be ready"
+elb-describe-instance-health --region us-west-1 --headers --lb ${loadBalancer}
+echo elb-describe-instance-health --region us-west-1 --headers --lb ${loadBalancer}
+echo "Loadbalancer ready"
+echo "Hosts:"
+
+for host in `cat hosts.txt | sed -e's%.* %%'`
+do
+    echo "\t${host}"
+done
+
+echo "Loadbalancer: ${loadBalancerDNSName}"
+echo "Loadbalancer not yet in service.  Use following command to check status:"
+echo "\telb-describe-instance-health --region us-west-1 --lb ${loadBalancer}"
+exit
--- create-keypair.sh
+++ create-keypair.sh
@@ -11,3 +11,9 @@ fi
 
 ec2-add-keypair $KEYNAME.keypair >  ~/.ssh/$KEYNAME.pem
 chmod 600  ~/.ssh/$KEYNAME.pem
+echo "Add the following to your environment:"
+echo "csh/tcsh:"
+echo setenv EC2_KEYPAIR_NAME $KEYNAME.keypair
+echo "sh/bash:"
+echo "export EC2_KEYPAIR_NAME=$KEYNAME.keypair"
+
